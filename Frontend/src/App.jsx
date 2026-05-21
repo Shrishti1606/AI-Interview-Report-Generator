f24b75e777e4ca97e413c159f0e6685df8d628aa
Shrishti1606 <shrishtidhiman04@gmail.com>
2026-05-21 15:13:35 +0530

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,8 +3,15 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from "./app.routes.jsx"
 import { AuthProvider } from './features/auth/auth.context.jsx'
 import { InterviewProvider }from './features/interview/interview.context.jsx'
+import { AuthContext } from './context/AuthContext'
 
 const App = () => {
+
+  const { loading } = useContext(AuthContext)
+
+  // ✅ Block ALL rendering until auth check is done
+  if (loading) return <main><h1>Loading...</h1></main>
+
   return (
     <AuthProvider>
       <InterviewProvider>
