3c5c93df359ac869a51122ed08059024fc4cf0a6
Shrishti1606 <shrishtidhiman04@gmail.com>
2026-05-21 15:36:04 +0530

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from "./app.routes.jsx"
 import { AuthProvider } from './features/auth/auth.context.jsx'
 import { InterviewProvider }from './features/interview/interview.context.jsx'
-import { AuthProvider } from '.features/auth/auth.Context.jsx'
 import './interview.scss'
 
 const App = () => {
