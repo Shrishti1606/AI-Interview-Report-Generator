baec9b6dae482c3b087d5faef63ba7e94dd856de
Shrishti1606 <shrishtidhiman04@gmail.com>
2026-09-08 19:12:36 +0530

    remove catch-all route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,28 +1,21 @@
-// updated
-
 require('dotenv').config()
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const path = require('path');
 
 const app = express();
 
 app.set('trust proxy', 1);
 
 app.use(express.json());
-
 app.use(cookieParser());
-
 app.use(cors({
     origin: ["http://localhost:5173", "https://prepbot-alpha.vercel.app"],
     credentials: true
 }));
 
-/*require all routes*/
 const authRouter = require('./routes/auth.routes')
 const interviewRouter = require('./routes/interview.routes') 
-
 
 app.use('/api/auth', authRouter);
 app.use('/api/interview', interviewRouter);
@@ -31,9 +24,4 @@ app.get('/api/health', (req, res) => {
     res.status(200).json({ status: 'ok' });
 });
 
-app.get('/{*path}', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-});
 
-
-module.exports = app;
