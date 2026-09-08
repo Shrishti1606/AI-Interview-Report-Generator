a40a2b1e6724d18c7fa119b1f448d4404f122c57
Shrishti1606 <shrishtidhiman04@gmail.com>
2026-09-08 20:23:10 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -4,18 +4,18 @@ const connectDB = require('./src/config/database')
 // const { resume, selfDescription, jobDescription } = require("./src/services/temp")
 // const generateInterviewReport = require("./src/services/ai.service");  
 
-connectDB()
-
 // generateInterviewReport({ resume, selfDescription, jobDescription })
+const PORT = process.env.PORT || 3000;
 
+connectDB()
 .then(() => {
-        console.log("DB connected") // 👈 check if this prints
-        app.listen(3000, () => {
-            console.log('server is running on port 3000');
-        });
-    })
-    .catch((err) => {
-        console.error("DB connection failed:", err) // 👈 or this
-        process.exit(1)
-    })
+    console.log("DB connected")
+    app.listen(PORT, () => {
+        console.log(`server is running on port ${PORT}`);
+    });
+})
+.catch((err) => {
+    console.error("DB connection failed:", err)
+    process.exit(1)
+})
 
